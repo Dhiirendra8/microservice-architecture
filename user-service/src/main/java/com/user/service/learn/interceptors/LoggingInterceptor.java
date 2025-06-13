2de76d041bd4b32b1f3fb6e8e6d23b31bf2ea2b0
Dhiirendra8 <dhiirendra8@gmail.com>
2025-06-13 23:04:49 +0530

    update

---
+++ user-service/src/main/java/com/user/service/learn/interceptors/LoggingInterceptor.java
@@ -0,0 +1,19 @@
+package com.user.service.learn.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.LocalDateTime;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Request URI: "+request.getRequestURI());
+        System.out.println("Timestamp: "+ LocalDateTime.now());
+        return true;
+    }
+}
